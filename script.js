e87efd24bd58cbe50fdb887d90419a3430de2078
VIOREL STANCIU <87269994+viorelstanciu@users.noreply.github.com>
2022-07-10 17:38:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//! === ADD HEADER STYLES ON SCROLL ===
+const headerScroll = () => {
+    const headerElement = document.querySelector('.header');
+    this.scrollY >= 300 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
+}
+
+window.addEventListener('scroll', headerScroll);
+// ! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
+const menuToggler = document.querySelector('#menu-toggler');
+const navbarMenu = document.querySelector('.navbar__menu');
+const toggleMenu = () => {
+    navbarMenu.classList.toggle('active');
+}
+
+menuToggler.addEventListener('click', toggleMenu);
+
+// --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
+const linksToggleMenu = (e) => {
+    if(e.target.classList.contains('navbar__list-link')) navbarMenu.classList.remove('active');
+}
+
+window.addEventListener('click', linksToggleMenu)
